953b898a576de52133360e5a8406cd81dbdc6c3e
Marcos Eduardo <markuzemb18@gmail.com>
2021-11-09 12:31:12 -0600

    Modificacion a la rama principal

--- EjercicioIncremental-Lunes/js/app.js
+++ EjercicioIncremental-Lunes/js/app.js
@@ -2,26 +2,27 @@
 
 let userName = document.getElementById("nombre-user");
 let dataUser = document.getElementById("data-user");
+let usuarios = [];
+let imagen_p = document.getElementById('img-principal');
+let imagen_m = document.getElementById('img-perfil');
+let nombre_user = document.getElementById('nombre-user');
+let nombre_user_p = document.getElementById('nombre-user-p');
 
 
 //URL de la API
 const API_URL = 'https://randomuser.me/api/';
 //const API_KEY = 'GQXN-X6N1-ZQ3D-4UPC';
 
-async function loadUser(){
+async function loadUser() {
     return (await fetch(`${API_URL}?results=10&inc=gender,name,email,phone,picture,dob`)).json();
 }
 
 
-document.addEventListener('DOMContentLoaded', async ()=>{
-    let users;
-    try{
-        users = await loadUser();
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        let users = await loadUser();
         let resultado = users.results;
-        let imagen_p = document.getElementById('img-principal');
-        let imagen_m = document.getElementById('img-perfil');
-        let nombre_user = document.getElementById('nombre-user');
-        let nombre_user_p = document.getElementById('nombre-user-p');
+
         imagen_p.src = `${Object.values(resultado[0].picture)[0]}`;
         imagen_m.src = `${Object.values(resultado[0].picture)[1]}`;
         nombre_user.innerHTML = `${Object.values(resultado[0].name).join(' ')}.`;
@@ -30,7 +31,7 @@ document.addEventListener('DOMContentLoaded', async ()=>{
         console.log(Object.values(resultado[0].name));
         console.log(Object.values(resultado[0].picture)[1]);
         console.log(users.results);
-    }catch(e){
+    } catch (e) {
         console.log('Ocurrio un error al solicitar los datos: ' + e.message);
     }
 });
