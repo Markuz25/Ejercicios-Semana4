a791ce6cf7caa6666efaf551b317146330f992f6
Marcos Eduardo <markuzemb18@gmail.com>
2021-11-09 18:55:05 -0600

    Se agregaron ;

--- EjercicioIncremental-Lunes/js/desarrollo.js
+++ EjercicioIncremental-Lunes/js/desarrollo.js
@@ -1,26 +1,25 @@
 const URL_API = `https://jsonplaceholder.typicode.com/users/`;
-const result = document.getElementById('resultado')
-const listItems = []
+const result = document.getElementById('resultado');
+const listItems = [];
 
 async function Datos() {
-  
-    const res = await fetch(URL_API)
-    const results = await res.json()
-     result.innerHTML = ''
-  
-    results.forEach(user => {
-      const li = document.createElement('li')
-      
-      listItems.push(li)
-  
-      li.innerHTML = `
+
+  const res = await fetch(URL_API);
+  const results = await res.json();
+  result.innerHTML = '';
+
+  results.forEach(user => {
+    const li = document.createElement('li');
+
+    li.innerHTML = `
           <div class="user-info">
             <h4>${user.name} ${user.username}</h4>
             <p>${user.email}</p>
             <p>${user.phone}</p>
           </div>
         `
-        result.appendChild(li)
-    })
-  }
-  Datos()
+    listItems.push(li);
+    result.appendChild(li);
+  })
+}
+Datos();
